4e0a67b8d031e76f5ca25942b02839cb11673a98
CArefiev <cristina.arefiev@endava.com>
2020-10-30 13:31:38 +0200

    Changes to Branch2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
 
+    public static void sum(Integer a, Integer b){
+
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
 
         System.out.println("Hello!");
