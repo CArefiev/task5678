48ef303c6571d8ebf51ca7ced6b77e3d42c2adf5
CArefiev <cristina.arefiev@endava.com>
2020-10-30 17:44:55 +0200

    Added all commits in one

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
 
-    public static void sum(Integer a, Integer b) {
-
-        System.out.println(a + b);
-    }
-
-    public static void dif(Integer a, Integer b){
-
-        System.out.println(a-b);
-    }
 
     public static void main(String[] args) {
 
         System.out.println("Hello!");
-
-        System.out.println("Hi!");
+        System.out.println("Hi");
+        System.out.println("Hello world");
     }
 }
