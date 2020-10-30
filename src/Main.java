e699bebed32c6194046e714ccc0a736dac930f65
CArefiev <cristina.arefiev@endava.com>
2020-10-30 17:39:04 +0200

    One commit to main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
-    public static void sum(Integer a, Integer b) {
-
-        System.out.println(a + b);
-    }
-    
 
     public static void main(String[] args) {
 
         System.out.println("Hello!");
-
         System.out.println("Hi!");
+        System.out.println("Hello world!");
+
     }
 }
